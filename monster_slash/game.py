e6f6ce10685d84f12ba733c4c25995e361efdeca
Michael Heidelberger <mheidelberger@NVC.local>
2017-04-27 11:23:51 -0500

    6.1 Moved play method into the "Game" class to simplify and create cleaner looking code

--- monster_slash/game.py
+++ monster_slash/game.py
@@ -1,46 +1,59 @@
 import random
 from actors import Player, Enemy
-input()
-def main():
-        print_intro()
-        play()
 
-def print_intro():
-    print('''
-        Monster Slash !!!
-        Ready Player One?
-        [Press Enter to Continue]
-    ''')
-    input()
 
-def play():
-    enemies = [
-        Enemy('Ogre',1),
-        Enemy('Imp',1)
-    ]
-    player = Player('Hercules',1)
-    while True:
-        next_enemy = random.choice(enemies)
-        cmd = input('You see a {}, [r]un, [a]ttack, [p]ass?\n'.format(next_enemy.kind))
-        if cmd == 'r':
-            print('{} runs away!'.format(player.name))
-        elif cmd == 'a':
-            print('{} attacks the {}'.format(player.name, next_enemy.kind))
-            if player.attacks(next_enemy):
-                enemies.remove(next_enemy)
-            else:
-                print('{} hides to plan the next move'.format(player.name))
-        elif cmd == 'p':
-            print('You are still thinking about your next move...')
-        else:
-            print('Please choose a valid option')
+class Game:
+    def __init__(self,player,enemies):
+        self.player = player
+        self.enemies = enemies
+
+    def main(self):
+            self.print_intro()
+            self.play()
+            #play()
+
+    def print_intro(self):
+        print('''
+            Monster Slash !!!
+            Ready Player One?
+            [Press Enter to Continue]
+            ''')
+        input()
+
+    def print_linebreak(self):
         print()
         print('*'*30)
         print()
 
-        if not enemies:
-            print('You have won! Congrats!')
-            break
+    def play(self):
+        while True:
+            next_enemy = random.choice(self.enemies)
+            cmd = input('You see a {}, [r]un, [a]ttack, [p]ass?\n'.format(next_enemy.kind))
+            if cmd == 'r':
+                print('{} runs away!'.format(self.player.name))
+            elif cmd == 'a':
+                print('{} attacks the {}'.format(self.player.name, next_enemy.kind))
+                if self.player.attacks(next_enemy):
+                    self.enemies.remove(next_enemy)
+                else:
+                    print('{} hides to plan the next move'.format(self.player.name))
+            elif cmd == 'p':
+                print('You are still thinking about your next move...')
+            else:
+                print('Please choose a valid option')
+            self.print_linebreak()
+            if not enemies:
+                print('You have won! Congrats!')
+                break
+
 
 if __name__ == '__main__':
-    main()
+    #main()
+    enemies = [
+        Enemy('Ogre',1),
+        Enemy('Imp',1)
+        ]
+    player = Player('Hercules',1)
+
+    Game(player, enemies).main()
+    Game(player, enemies).main()
